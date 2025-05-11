89fa0d761fc3f24b20009b4d2be5e90b9a0953de
tienson05 <tienson050429@gmail.com>
2025-05-11 20:47:11 +0700

    hehe

--- CaffeeShopManagement/src/Main/App.java
+++ CaffeeShopManagement/src/Main/App.java
@@ -5,7 +5,7 @@ import Db.DBConnection;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main {
+public class App {
     public static void main(String[] args) throws SQLException {
         try (Connection conn = DBConnection.getConnection()) {
             System.out.println("Connected to database successfully!");
